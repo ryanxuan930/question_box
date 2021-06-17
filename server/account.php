8ab146d3556026046e8eba88844895f93eecda7e
ryanxuan930 <anakinchung930@gmail.com>
2021-06-17 16:57:19 +0800

    account

---
+++ server/account.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$account = $_SESSION["account"];
+header("Access-Control-Allow-Origin: https://ryanxuan930.github.io");
+include("dbclass.php");
+$db = new database("twitter");
+$db->table("admin");
+$result = $db->select_where(0,"account = '{$account}'");
+$array = array();
+while($row = $result->fetch_assoc()){
+    $array["account"] = $row["account"];
+    $array["name"] = $row["name"];
+}
+$obj = json_encode($array);
+echo $obj;
+?>
--- server/login.php
+++ server/login.php
@@ -13,6 +13,7 @@ if($result->num_rows){
         $hash = $row["password"];
     }
     if(password_verify($password,$hash)){
+        $_SESSION["account"] = $account;
         echo '<script>location.href="https://ryanxuan930.github.io/question_box/admin/main.html";</script>';
     }else{
         echo '<script>alert("密碼錯誤");location.href="https://ryanxuan930.github.io/question_box/admin/login.html";</script>';

7c9e9d2456c02757a76a0328b59628000ad66062
ryanxuan930 <anakinchung930@gmail.com>
2021-06-17 16:10:59 +0800

    Update login.php

--- server/login.php
+++ server/login.php
@@ -13,11 +13,11 @@ if($result->num_rows){
         $hash = $row["password"];
     }
     if(password_verify($password,$hash)){
-        echo '<script>location.href="https://ryanxuan930.github.io/question_box/admin/main.html;"</script>';
+        echo '<script>location.href="https://ryanxuan930.github.io/question_box/admin/main.html";</script>';
     }else{
-        echo '<script>alert("密碼錯誤");location.href="https://ryanxuan930.github.io/question_box/admin/login.html;"</script>';
+        echo '<script>alert("密碼錯誤");location.href="https://ryanxuan930.github.io/question_box/admin/login.html";</script>';
     }
 }else{
-    echo '<script>alert("帳號錯誤");location.href="https://ryanxuan930.github.io/question_box/admin/login.html;"</script>';
+    echo '<script>alert("帳號錯誤");location.href="https://ryanxuan930.github.io/question_box/admin/login.html";</script>';
 }
 ?>

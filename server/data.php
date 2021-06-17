881dfbd042d1252c3db58f165bac704ccf244662
ryanxuan930 <anakinchung930@gmail.com>
2021-06-17 15:57:25 +0800

    admin

--- server/data.php
+++ server/data.php
@@ -1,11 +1,18 @@
 <?php
 include("database.php");
 header("Access-Control-Allow-Origin: https://ryanxuan930.github.io");
-$content = $_POST["data"];
+if(!isset($_POST["name"])){
+    echo "請填寫暱稱！";
+}else if(!isset($_POST["data"])){
+    echo "請填寫問題！";
+}else{
+    $content = $_POST["data"];
+    $name = $_POST["name"];
+}
 $conn->select_db("twitter");
 $time = date("Y-m-d H:i:s");
-$stmt = $conn->prepare("INSERT INTO data (timestamp, question) VALUES (?,?)");
-$stmt->bind_param("ss",$time,$content);
+$stmt = $conn->prepare("INSERT INTO data (timestamp, name, question) VALUES (?,?,?)");
+$stmt->bind_param("sss",$time,$name,$content);
 $stmt->execute();
 if($conn->error){
     echo $conn->error;
---
+++ server/login.php
@@ -0,0 +1,25 @@
+<?php
+echo password_hash("Snake900930");
+include("database.php");
+$conn->select_db("twitter");
+$account = $_POST["account"];
+$password = $_POST["password"];
+$stmt = $conn->prepare("SELECT * FROM admin WHERE account=?");
+$stmt->bind_param("s",$account);
+$stmt->execute();
+$result = $stmt->get_result();
+if($result->num_rows){
+    while($row = $result->fetch_assoc()){
+        $hash = $row["password"];
+    }
+    if(password_verify($password,$hash)){
+        header("Location: https://ryanxuan930.github.io/question_box/admin/main.html");
+    }else{
+        echo "密碼錯誤";
+        header("Location: https://ryanxuan930.github.io/question_box/admin/login.html");
+    }
+}else{
+    echo "帳號錯誤";
+    header("Location: https://ryanxuan930.github.io/question_box/admin/login.html");
+}
+?>
